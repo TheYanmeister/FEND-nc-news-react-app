f1cccd1d6d90a5ec425760a3572356510037872d
TheYanmeister <yannickvdh1@gmail.com>
2019-07-19 12:22:22 +0100

    refactored articlecard for best practice

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,34 +1,34 @@
 import React from "react";
 import { Link } from "@reach/router";
 
-const formatArticleCard = props => {
-  return props.articles.map(article => {
-    return (
-      <section key={article.article_id} className="articleCards">
-        <h3 className="articleCards_title">
-          In {article.topic}: {article.title}
-        </h3>
-        <p className="articleCards_body">
-          {article.body.slice(0, article.body.indexOf(".") + 1)}..{" "}
-        </p>
-        <p>
-          {" "}
-          <Link to={`/articles/${article.article_id}`}>
-            (click to read the full article)
-          </Link>
-        </p>
-        <p className="articleCards_author">Author: {article.author}</p>
-        <br />
-        <p>
-          Comment count: {article.comment_count} | Votes: {article.votes}
-        </p>
-      </section>
-    );
-  });
-};
-
 const ArticleCard = props => {
-  return <section>{formatArticleCard(props)}</section>;
+  return (
+    <section>
+      {props.articles.map(article => {
+        return (
+          <section key={article.article_id} className="articleCards">
+            <h3 className="articleCards_title">
+              In {article.topic}: {article.title}
+            </h3>
+            <p className="articleCards_body">
+              {article.body.slice(0, article.body.indexOf(".") + 1)}..{" "}
+            </p>
+            <p>
+              {" "}
+              <Link to={`/articles/${article.article_id}`}>
+                (click to read the full article)
+              </Link>
+            </p>
+            <p className="articleCards_author">Author: {article.author}</p>
+            <br />
+            <p>
+              Comment count: {article.comment_count} | Votes: {article.votes}
+            </p>
+          </section>
+        );
+      })}
+    </section>
+  );
 };
 
 export default ArticleCard;
